84eb7ab2f8a90dcc20caf1d4be1e8057bccb9971
ShutUpPetey <matthewinder@gmail.com>
2016-07-26 17:51:43 -0500

    adding notifier

--- pogom/models.py
+++ pogom/models.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import logging
-
+import notifier
 import os
 from peewee import Model, MySQLDatabase, SqliteDatabase, InsertQuery, IntegerField,\
                    CharField, DoubleField, BooleanField, DateTimeField,\
