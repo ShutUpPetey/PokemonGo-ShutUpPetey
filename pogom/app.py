eb9c0e75276656e12c5f9b644dfc1ce9d18a3c22
ShutUpPetey <matthewinder@gmail.com>
2016-07-27 11:03:08 -0500

    Changing Mobile

--- pogom/app.py
+++ pogom/app.py
@@ -97,36 +97,43 @@ class Pogom(Flask):
     def list_pokemon(self):
         # todo: check if client is android/iOS/Desktop for geolink, currently only supports android
         pokemon_list = []
+		
+		
 
         # Allow client to specify location
         lat = request.args.get('lat', config['ORIGINAL_LATITUDE'], type=float)
         lon = request.args.get('lon', config['ORIGINAL_LONGITUDE'], type=float)
+		
+	exclude_list = request.args.get('exclude',default=())
+	print exclude_list
+		
         origin_point = LatLng.from_degrees(lat, lon)
-
+		
         for pokemon in Pokemon.get_active(None, None, None, None):
-            pokemon_point = LatLng.from_degrees(pokemon['latitude'], pokemon['longitude'])
-            diff = pokemon_point - origin_point
-            diff_lat = diff.lat().degrees
-            diff_lng = diff.lng().degrees
-            direction = (('N' if diff_lat >= 0 else 'S') if abs(diff_lat) > 1e-4 else '') + (
-                ('E' if diff_lng >= 0 else 'W') if abs(diff_lng) > 1e-4 else '')
-            entry = {
-                'id': pokemon['pokemon_id'],
-                'name': pokemon['pokemon_name'],
-                'card_dir': direction,
-                'distance': int(origin_point.get_distance(pokemon_point).radians * 6366468.241830914),
-                'time_to_disappear': '%d min %d sec' % (divmod((pokemon['disappear_time']-datetime.utcnow()).seconds, 60)),
-                'disappear_time': pokemon['disappear_time'],
-                'latitude': pokemon['latitude'],
-                'longitude': pokemon['longitude']
-            }
-            pokemon_list.append((entry, entry['distance']))
+			if str(pokemon['pokemon_id']) not in exclude_list:
+				pokemon_point = LatLng.from_degrees(pokemon['latitude'], pokemon['longitude'])
+				diff = pokemon_point - origin_point
+				diff_lat = diff.lat().degrees
+				diff_lng = diff.lng().degrees
+				direction = (('N' if diff_lat >= 0 else 'S') if abs(diff_lat) > 1e-4 else '') + (
+					('E' if diff_lng >= 0 else 'W') if abs(diff_lng) > 1e-4 else '')
+				entry = {
+					'id': pokemon['pokemon_id'],
+					'name': pokemon['pokemon_name'],
+					'card_dir': direction,
+					'distance': int(origin_point.get_distance(pokemon_point).radians * 6366468.241830914),
+					'time_to_disappear': '%d min %d sec' % (divmod((pokemon['disappear_time']-datetime.utcnow()).seconds, 60)),
+					'disappear_time': pokemon['disappear_time'],
+					'latitude': pokemon['latitude'],
+					'longitude': pokemon['longitude']
+				}
+				pokemon_list.append((entry, entry['distance']))
         pokemon_list = [y[0] for y in sorted(pokemon_list, key=lambda x: x[1])]
         return render_template('mobile_list.html',
                                pokemon_list=pokemon_list,
                                origin_lat=lat,
                                origin_lng=lon)
-
+		
 
 class CustomJSONEncoder(JSONEncoder):
 
